c9344b7a18310d4f6162d535d133ee8d8c646df4
Rubber-Sole <4d1schooldyl@gmail.com>
2022-08-05 13:19:32 -0600

    fully functioning + fixed up percentage formatting and added references

--- Youngdahl-HealthCalculator.py
+++ Youngdahl-HealthCalculator.py
@@ -22,7 +22,7 @@ print('\nPlease enter the following values for the user . . .\n')
 ht, wt, age, hrate = 0, 0, 0, 0
 metrics = [ht, wt, age, hrate]
 
-while metrics[0 or 1 or 2 or 3] == 0:  # still need to figure out how to make this less clunky...
+while metrics[0 or 1 or 2 or 3] == 0:  # (1) still need to figure out how to make this less clunky...
     try:
         ht = int(input('Height, in inches: '))
     except ValueError:
@@ -53,27 +53,27 @@ def bmi():  # final_bmi = weight(kg) / height(m^2)
     meters = ht * 0.0254  # convert inches to meters
     meters_sq = meters ** 2  # square our value in compliance with BMI formula
     kg = wt * 0.45359237  # convert pounds to kilograms
-    bmi.final_bmi = kg / meters_sq
-    bmi.final_bmi = round(bmi.final_bmi, 2)
+    final_bmi = kg / meters_sq
+    final_bmi = round(final_bmi, 2)
 
-    bmi.finalmetrics = [bmi.final_bmi]
+    finalmetrics = [final_bmi]  # did not end up serving a use here, but figured it'd be nice to hold list for bmi info
 
-    if bmi.final_bmi < 18.5:
-        bmi.underweight = 'Underweight', True
-        list.append(bmi.finalmetrics, bmi.underweight[1])
-        print('\nResults...\nYour BMI is:', bmi.final_bmi, '--', bmi.underweight[0])
-    elif 18.5 <= bmi.final_bmi <= 24.9:
-        bmi.normal = 'Normal weight', True
-        list.append(bmi.finalmetrics, bmi.normal[1])
-        print('\nResults...\nYour BMI is:', bmi.final_bmi, '--', bmi.normal[0])
-    elif 25 <= bmi.final_bmi <= 29.9:
-        bmi.overweight = 'Overweight', True
-        list.append(bmi.finalmetrics, bmi.overweight[1])
-        print('\nResults...\nYour BMI is:', bmi.final_bmi, '--', bmi.overweight[0])
-    elif bmi.final_bmi > 30:
-        bmi.obese = 'Obese', True
-        list.append(bmi.finalmetrics, bmi.obese[1])
-        print('\nResults...\nYour BMI is:', bmi.final_bmi, '--', bmi.obese[0])
+    if final_bmi < 18.5:
+        underweight = 'Underweight', True
+        list.append(finalmetrics, underweight[1])
+        print('\nResults...\nYour BMI is:', final_bmi, '--', underweight[0])
+    elif 18.5 <= final_bmi <= 24.9:
+        normal = 'Normal weight', True
+        list.append(finalmetrics, normal[1])
+        print('\nResults...\nYour BMI is:', final_bmi, '--', normal[0])
+    elif 25 <= final_bmi <= 29.9:
+        overweight = 'Overweight', True
+        list.append(finalmetrics, overweight[1])
+        print('\nResults...\nYour BMI is:', final_bmi, '--', overweight[0])
+    elif final_bmi > 30:
+        obese = 'Obese', True
+        list.append(finalmetrics, obese[1])
+        print('\nResults...\nYour BMI is:', final_bmi, '--', obese[0])
     else:
         print('\nAn unknown error has occurred, try running again.')
 
@@ -96,18 +96,20 @@ def karvonen():
     tr = .50  # target rate
     print('\nExercise intensity heart rates:\n', 'Intensity level:\tMax heart rate', )
 
-    for tr in np.arange(.50, 1, .05):  # () Utilize numpy to increment the TR by 5% until all values are found
+    for tr in np.arange(.50, 1, .05):  # (3) Utilize numpy to increment the TR by 5% until all values are found
         mtz = hrr * tr  # max target zone
         ttz = 0  # target training zone
         ttz = mtz + metrics[3]  # hrate
-        print(round(tr, 2), '%\t\t---\t\t', int(ttz), 'BPM')
+        percent = "{:.0%}".format(tr)  # (4)
+        print(percent, '\t\t---\t\t', int(ttz), 'BPM')
 
 
 karvonen()
 
 
 """
-() https://www.w3schools.com/python/python_try_except.asp
-() https://www.w3schools.com/python/python_variables_global.asp
-() https://stackoverflow.com/questions/477486/how-do-i-use-a-decimal-step-value-for-range
+(1) https://www.w3schools.com/python/python_try_except.asp
+(2) https://www.w3schools.com/python/python_variables_global.asp  // did not end up utilizing global variables
+(3) https://stackoverflow.com/questions/477486/how-do-i-use-a-decimal-step-value-for-range
+(4) https://www.adamsmith.haus/python/answers/how-to-format-a-number-as-a-percentage-in-python
 """
